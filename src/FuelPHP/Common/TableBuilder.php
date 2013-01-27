b3f5c832e6927858b97ddba37fd39fd0ca243c3a
stevewest <uruwolf@gmail.com>
2013-01-27 10:37:27 +0000

    Adds comments and does a little refactoring

--- src/FuelPHP/Common/TableBuilder.php
+++ src/FuelPHP/Common/TableBuilder.php
@@ -11,7 +11,7 @@
 namespace FuelPHP\Common;
 
 /**
- * 
+ * Deals with constructing table structures.
  *
  * @package FuelPHP\Common
  * @since   2.0.0
@@ -20,27 +20,64 @@ namespace FuelPHP\Common;
 class TableBuilder
 {
 	
+	/**
+	 * @var array Contains cconstructed rows
+	 */
 	protected $_rows = array();
 	
+	/**
+	 * @var Row The row that new cells will be added to
+	 */
 	protected $_currentRow = null;
 	
+	/**
+	 * Adds a Cell to the current Row.
+	 * 
+	 * @param mixed $content Anything that is not a Cell will be added as content to a new Cell
+	 * @return \FuelPHP\Common\TableBuilder For method chaining
+	 */
 	public function addCell($content)
-	{
-		if ( is_null($this->_currentRow) )
+	{	
+		$currentRow = $this->getCurrentRow();
+		//If we have been given a Cell then just add it, else create a new cell
+		if ($content instanceof Cell)
 		{
-			$this->createRow();
+			$currentRow[] = $content;
+		}
+		else
+		{
+			$currentRow[] = $this->constructCell($content);
 		}
 		
-		$this->_currentRow[] = new Cell($content);
-		
+		//Return current object for method chaining
 		return $this;
 	}
 	
+	/**
+	 * Creates a new Cell with the given content.
+	 * 
+	 * @param mixed $content The content for the new Cell
+	 * @return \FuelPHP\Common\Cell
+	 */
+	protected function constructCell($content=null)
+	{
+		return new Cell($content);
+	}
+	
+	/**
+	 * Creates a new Row object and assigns it as the currently active row.
+	 */
 	protected function createRow()
 	{
 		$this->_currentRow = new Row;
 	}
 
+	/**
+	 * Adds the Row that's currently being constructed to the list of finished
+	 * Rows.
+	 * 
+	 * @return \FuelPHP\Common\TableBuilder
+	 */
 	public function addRow()
 	{
 		$this->_rows[] = $this->_currentRow;
@@ -49,8 +86,29 @@ class TableBuilder
 		return $this;
 	}
 	
+	/**
+	 * Returns a list of all currently consructed Rows
+	 * 
+	 * @return array
+	 */
 	public function getRows()
 	{
 		return $this->_rows;
+	}
+	
+	/**
+	 * Gets the currently active row. The row will not be added until addRow()
+	 * is called.
+	 * 
+	 * @return type
+	 */
+	public function getCurrentRow()
+	{
+		if (is_null($this->_currentRow))
+		{
+			$this->createRow();
+		}
+		
+		return $this->_currentRow;
 	}
 }
