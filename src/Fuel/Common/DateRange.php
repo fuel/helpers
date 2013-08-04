0dc497cbeef2a85ff578962ba8d3d98660895713
WanWizard <wanwizard@wanwizard.eu>
2013-08-04 20:06:04 +0200

    make a DateRange object a bit more flexible

--- src/Fuel/Common/DateRange.php
+++ src/Fuel/Common/DateRange.php
@@ -12,6 +12,8 @@ namespace Fuel\Common;
 
 use DatePeriod;
 use ArrayAccess;
+use DateInterval;
+use DateTime;
 
 /**
  * DateRange Class
@@ -23,6 +25,38 @@ use ArrayAccess;
  */
 class DateRange extends DatePeriod implements ArrayAccess
 {
+	/**
+	 * Make the DatePeriod constructor more flexible
+	 */
+	public function __construct($start, $interval, $end, $options = null)
+	{
+		// make sure $start is a DateTime object
+		if ( ! $start instanceOf DateTime)
+		{
+			$start = new DateTime($start);
+		}
+
+		// make sure $interval is a DateInterval object
+		if ( ! $interval instanceOf DateInterval)
+		{
+			$interval = DateInterval::createFromDateString($interval);
+		}
+
+		// make sure $end is a DateTime object
+		if ( ! $end instanceOf DateTime)
+		{
+			if (is_numeric($end) and $end > 1000)
+			{
+				$end = new DateTime('@'.$end);
+			}
+			elseif (is_string($end))
+			{
+				$end = new DateTime($end);
+			}
+		}
+
+		call_user_func_array('parent::__construct', array($start, $interval, $end, $options));
+	}
 	/**
 	 * Not implemented
 	 */
