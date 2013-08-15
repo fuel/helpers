e037f059dd3ad54d704da29ebc8572634db435de
WanWizard <wanwizard@wanwizard.eu>
2013-08-15 23:50:44 +0200

    removed the bootstap file; added a FuelServiceProvider

--- src/Fuel/Common/Providers/FuelServiceProvider.php
+++ src/Fuel/Common/Providers/FuelServiceProvider.php
@@ -8,20 +8,18 @@
  * @link       http://fuelphp.com
  */
 
-namespace Fuel\Common;
+namespace Fuel\Common\Providers;
 
 use Fuel\Dependency\ServiceProvider;
 
 /**
- * ServicesProvider class
- *
- * Defines the services published by this namespace to the DiC
+ * FuelPHP ServiceProvider class for this package
  *
  * @package  Fuel\Common
  *
  * @since  2.0.0
  */
-class ServicesProvider extends ServiceProvider
+class FuelServiceProvider extends ServiceProvider
 {
 	/**
 	 * @var  array  list of service names provided by this provider
@@ -36,31 +34,55 @@ class ServicesProvider extends ServiceProvider
 		// \Fuel\Common\DataContainer
 		$this->register('datacontainer', function ($dic, Array $data = array(), $readOnly = false)
 		{
-			return new DataContainer($data, $readOnly);
+			return $dic->resolve('Fuel\Common\DataContainer', array($data, $readOnly));
 		});
 
 		// \Fuel\Common\CookieJar
 		$this->register('cookiejar', function ($dic, Array $config = array(), Array $data = array())
 		{
-			return new CookieJar($config, $data);
+			return $dic->resolve('Fuel\Common\CookieJar', array($config, $data));
 		});
 
 		// \Fuel\Common\Format
 		$this->register('format', function ($dic, $data = null, $from_type = null, Array $config = array(), $input = null)
 		{
-			return new Format($data, $from_type, $config, $input);
+			// get the format config
+			$stack = $this->container->resolve('requeststack');
+			if ($request = $stack->top())
+			{
+				$instance = $request->getApplication()->getConfig();
+			}
+			else
+			{
+				$instance = $this->container->resolve('application.main')->getConfig();
+			}
+			$config = \Arr::merge($instance->load('format', true), $config);
+
+			return $dic->resolve('Fuel\Common\Format', array($data, $from_type, $config, $input));
 		});
 
 		// \Fuel\Common\Date
 		$this->register('date', function ($dic, $time = "now", $timezone = null, Array $config = array())
 		{
-			return new Date($time, $timezone, $config);
+			// get the date config
+			$stack = $this->container->resolve('requeststack');
+			if ($request = $stack->top())
+			{
+				$instance = $request->getApplication()->getConfig();
+			}
+			else
+			{
+				$instance = $this->container->resolve('application.main')->getConfig();
+			}
+			$config = \Arr::merge($instance->load('date', true), $config);
+
+			return $dic->resolve('Fuel\Common\Date', array($time, $timezone, $config));
 		});
 
 		// \Fuel\Common\Num
 		$this->register('num', function ($dic, Array $config = array(), Array $byteUnits = array())
 		{
-			return new Num($config, $byteUnits);
+			return $dic->resolve('Fuel\Common\Num', array($config, $byteUnits));
 		});
 	}
 }
